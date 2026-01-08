a3768cdd1eb0ad1757e764940272b1cee2c0067a
Yauheniya Mastavaya <jk.mostovaya@gmail.com>
2026-01-08 13:12:18 +0400

    feat: solve getAllResult

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -123,8 +123,10 @@ function getAllOrNothing(promises) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with [1, null, 3]
  */
-function getAllResult(/* promises */) {
-  throw new Error('Not implemented');
+function getAllResult(promises) {
+  return Promise.all(
+    promises.map((promise) => Promise.resolve(promise).catch(() => null))
+  );
 }
 
 /**
